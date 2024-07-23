8ca6e7d42cf2a03a666149b0c45d7c0f99e3fe87
imoleFolio <dremkay71@gmail.com>
2024-07-24 00:24:20 +0100

    task 3: linter done for utils.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  Promise.all([photo, user])
     .then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
