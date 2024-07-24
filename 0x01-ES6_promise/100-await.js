dbfe5d3c6e5955c70680bc5b3582730a437e1774
imoleFolio <dremkay71@gmail.com>
2024-07-24 13:05:54 +0100

    task 100: linting done

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploadUser(params) {
+export default async function asyncUploadUser() {
   try {
     const user = await createUser();
     const photo = await uploadPhoto();
-    return {photo, user};    
+    return { photo, user };
   } catch (error) {
-    return { photo: null, user: null, }    
-  }  
+    return { photo: null, user: null };
+  }
 }
