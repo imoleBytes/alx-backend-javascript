ac79f2655f538c5c22f83d600d89107dfc37bc98
imoleFolio <dremkay71@gmail.com>
2024-07-27 10:03:45 +0100

    task

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,7 +2,8 @@ import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   const sizes = [19, 20, 34];
-
   const roomArray = [];
-  return sizes.forEach((val) => roomArray.push(new ClassRoom(val)));
+
+  sizes.forEach((val) => roomArray.push(new ClassRoom(val)));
+  return roomArray;
 }
