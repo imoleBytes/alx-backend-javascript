2e5db63327c79b957a9177dd88f2eb717c31a791
imoleFolio <dremkay71@gmail.com>
2024-09-19 13:41:40 +0100

    task 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,31 +1,40 @@
-const assert = require("assert");
+const expect = require("chai").expect;
 const { it, describe } = require("mocha");
-const calculateNumber = require("./1-calcul");
+const calculateNumber = require("./2-calcul_chai");
 
-describe("calculateNumber()", function() {
-
-    it(`checking for positive numbers`, function() {
+describe('calculateNumber', () => {
+  describe("SUM", function() {
+    it(`checking SUM for positive numbers`, function() {
       const res = calculateNumber("SUM", 1, 2);
-      assert.strictEqual(res, 3);
+      expect(res).to.equal(3);
     });
+    it(`checking SUM for positive numbers`, function() {
+      const res = calculateNumber("SUM", 1, 52);
+      expect(res).to.equal(53);
+    });
+  });
+  describe('SUBTRACT', () => {
     it(`checking for floats`, function() {
       const res = calculateNumber("SUBTRACT", 1.4, 2.2);
-      assert.strictEqual(res, -1);
+      expect(res).to.equal(-1);
     });
     it(`checking subrtarct floats`, function() {
       const res = calculateNumber("SUBTRACT", 4.9, 2.7);
-      assert.strictEqual(res, 2);
+      expect(res).to.equal(2);
     });
+  })
+  describe('DIVIDE', () => {
     it(`checking for dividing positive integers`, function() {
       const res = calculateNumber("DIVIDE", 4, 2);
-      assert.strictEqual(res, 2);
+      expect(res).to.equal(2);
     });
     it(`checking for 0 denominator`, function() {
       const res = calculateNumber("DIVIDE", 1.7, 0);
-      assert.strictEqual(res, "Error");
+      expect(res).to.equal("Error");
     });
     it(`checking if numbers round`, function() {
       const res = calculateNumber("DIVIDE", 1.4, 4.6);
-      assert.strictEqual(res, 0.2);
+      expect(res).to.equal(0.2);
     });
+  });
 });
