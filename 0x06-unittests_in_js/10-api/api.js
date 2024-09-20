22e16ca6bf0589064a9a926ce2785210a458973d
imoleFolio <dremkay71@gmail.com>
2024-09-20 11:14:34 +0100

    task 10 o

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = 7865;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
@@ -12,26 +14,24 @@ app.get('/cart/:id([0-9]+)', (req, res) => {
 });
 
 app.get('/available_payments', (req, res) => {
-  res.set("Content-Type", "application/json");
-  const payMethods = {
-payment_methods: {
+    res.set("Content-Type", "application/json");
+    const payMethods = {
+      payment_methods: {
         credit_cards: true,
         paypal: false
-}
-  }
-  res.send(payMethods);
+      }
+    }
+    res.send(payMethods);
 });
 
 app.post('/login', (req, res) => {
-  const userName = req.body.userName;
-  if (userName) {
-res.send(`Welcome ${userName}`);
-  } else {
-res.status(404).send();
-  }
+    const userName = req.body.userName;
+    if (userName) {
+	res.send(`Welcome ${userName}`);
+    } else {
+	res.status(404).send();
+    }
 });
-
-
 
 app.listen(PORT, () => {
     console.log(`API available on localhost port ${PORT}`);
